7d8a4f00cf6294a56e8db3c78815d3430687f679
deb0890 <deborahf1@hotmail.com>
2022-02-27 19:04:27 +0000

    PromptButton component set up

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './Header';
 export { default as PromptForm} from './PromptForm';
-export { default as CategorySelect } from './CategorySelect'
+export { default as CategorySelect } from './CategorySelect';
+export { default as PromptButton } from './PromptButton';
