aa24103647a245f3da2ea2ffdfdcc39a3db643de
deb0890 <deborahf1@hotmail.com>
2022-02-27 18:53:22 +0000

    change component Form to PromptForm

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as Header } from './Header';
-export { default as PromptForm} from './Form';
+export { default as PromptForm} from './PromptForm';
 export { default as CategorySelect } from './CategorySelect'
