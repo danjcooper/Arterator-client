467972fdd027acd746032251a53e47a4c7770532
deb0890 <deborahf1@hotmail.com>
2022-02-27 19:23:51 +0000

    delete button component

--- src/components/PromptButton/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './style.css'
-
-const PromptButton = () => {
-
-
-} 
-
-export default PromptButton;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Header } from './Header';
 export { default as PromptForm} from './PromptForm';
 export { default as CategorySelect } from './CategorySelect';
-export { default as PromptButton } from './PromptButton';
