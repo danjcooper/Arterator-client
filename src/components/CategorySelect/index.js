4d735141f983ac2e419acfde40dd7406b936a0c6
deb0890 <deborahf1@hotmail.com>
2022-03-31 00:24:06 +0100

    add onchange function to select element

--- src/components/CategorySelect/index.js
+++ src/components/CategorySelect/index.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 
 const CategorySelect = ({category}) => {
+   const [ option, setOption ] = useState(category.tag)
+
 
     return (
         <>
             <label>{category.categoryname}</label>
-            <select id={category.categoryname}>
+            <select id={category.categoryname} onChange={(e) => setOption(e.target.value)}>
                 {category.tags.map((tag, index) => <option key={index}>{tag}</option> )}
             </select> 
         </>
