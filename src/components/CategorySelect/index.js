6426e058b7bfa6ae3c24ec25a91e8cae031986b9
deb0890 <deborahf1@hotmail.com>
2022-04-05 20:53:10 +0100

    add select attr to handle option change

--- src/components/CategorySelect/index.js
+++ src/components/CategorySelect/index.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react'
 import './style.css'
 
-const CategorySelect = ({category}) => {
-   const [ option, setOption ] = useState(category.tag)
+const CategorySelect = ({category, onChange}) => {
 
 
     return (
         <>
             <label>{category.categoryname}</label>
-            <select id={category.categoryname} onChange={(e) => setOption(e.target.value)}>
+            <select id={category.categoryname} onChange={onChange}>
                 {category.tags.map((tag, index) => <option key={index}>{tag}</option> )}
             </select> 
         </>
