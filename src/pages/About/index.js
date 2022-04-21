929d8720381dece973837d1cd15eac4a1f223449
deb0890 <deborahf1@hotmail.com>
2022-04-21 21:52:31 +0100

    create pages folder, add about, random prompts, index

---
+++ src/pages/About/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './style.css' 
+
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p> Art prompt is about giving artists, whether budding or professional, the inspiration to create when it 
+        can be hard to do so. Our hope is that the artists get that spark they need for the next modern masterpiece! </p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/RandomPrompts/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+const RandomPrompts = () => {
+
+
+
+}
+
+export default RandomPrompts;
