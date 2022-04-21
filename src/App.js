7ae83391b47313b7673fdce3a56837ace9b91063
deb0890 <deborahf1@hotmail.com>
2022-04-21 19:02:43 +0100

    add about path the App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import * as React from 'react';
-import { Header, PromptForm, PromptResult } from './components';
+import { Header, PromptForm, PromptResult, Nav, About } from './components';
 import { Routes, Route } from 'react-router-dom';
 
 
 const App = () => {
   return (
     <>
+        <Nav/>
         <Header />
           <Routes>
             <Route path='/' element={<PromptForm/>} />
-            <Route path='/promptresult' element={<PromptResult />} /> {/* index? */}
-          </ Routes>
+            <Route path='/promptresult' element={<PromptResult />} /> 
+            <Route path='/about' element={<About />} /> 
+          </Routes>
     </>
   );
 };
