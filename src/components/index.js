108ae6c868409991919e3d9d50f52036f71d118c
deb0890 <deborahf1@hotmail.com>
2022-04-21 22:04:56 +0100

    remove about export

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,3 @@ export { default as PromptForm} from './PromptForm';
 export { default as CategorySelect } from './CategorySelect';
 export { default as PromptResult } from './PromptResult';
 export { default as Nav } from './Nav';
-export { default as About } from './About';
