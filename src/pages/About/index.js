7b022a316869de76982de80e251908caa325ebd3
deb0890 <deborahf1@hotmail.com>
2022-04-26 20:18:29 +0100

    add style attribute module

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import './style.css' 
+import style from "./style.module.css"
 
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={style.resultsContainer} >
+      <h1> About</h1>
       <p> Art prompt is about giving artists, whether budding or professional, the inspiration to create when it 
         can be hard to do so. Our hope is that the artists get that spark they need for the next modern masterpiece! </p>
     </div>
