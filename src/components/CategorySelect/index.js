2d465782ed553bd94294763b5d2182d10fcdadc3
deb0890 <deborahf1@hotmail.com>
2022-04-26 20:27:05 +0100

    add please select option to categoryselect

--- src/components/CategorySelect/index.js
+++ src/components/CategorySelect/index.js
@@ -8,6 +8,7 @@ const CategorySelect = ({category, onChange}) => {
         <>
             <label>{category.categoryname}</label>
             <select id={category.categoryname} onChange={onChange}>
+                <option disabled selected>Please select</option>
                 {category.tags.map((tag, index) => <option key={index}>{tag}</option> )}
             </select> 
         </>
