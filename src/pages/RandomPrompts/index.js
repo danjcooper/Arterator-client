8b9b192f40e45ee7c97f38de34c85a1ce06f05fb
deb0890 <deborahf1@hotmail.com>
2022-06-05 15:52:11 +0100

    Randomprompts page?

--- src/pages/RandomPrompts/index.js
+++ src/pages/RandomPrompts/index.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import './style.css'
+import React, { useEffect, useState } from "react";
+import { RandomImage } from "../../components";
+import axios from "axios";
+import "./style.css";
+
+const imagesURL =
+  "https://api.unsplash.com/photos/random?client_id=KlqKTIHfc9rS-ilCMxEZx_0-XF5g8PKXySEU5IiCVOM";
 
 const RandomPrompts = () => {
+  const [randomImage, setRandomImage] = useState([]);
 
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(imagesURL);
+      // console.log(response.data);
+      // const data = await response.data;
+      setRandomImage(response.data);
+    };
+    getData();
+  }, []);
 
-
-}
+  return <>{<RandomImage key={randomImage.id} props={randomImage} />}</>;
+};
 
 export default RandomPrompts;
