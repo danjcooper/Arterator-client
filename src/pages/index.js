32eb4f3c306c8ed8619c41711039b4f68f2668f9
deb0890 <deborahf1@hotmail.com>
2022-06-05 15:40:24 +0100

    uncomment randomprompts export

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,2 @@
-export { default as About } from './About';
-// export { default as RandomPrompts } from './RandomPrompts';
+export { default as About } from "./About";
+export { default as RandomPrompts } from "./RandomPrompts";
