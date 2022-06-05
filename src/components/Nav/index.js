87df24bc7935ed1612b968198f486888b04a399a
deb0890 <deborahf1@hotmail.com>
2022-06-05 15:05:53 +0100

    prettified

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { NavLink, useNavigate } from 'react-router-dom'
-import './style.css';
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import "./style.css";
 
 const Nav = () => {
-
   // const navigate = useNavigate;
-  
+
   return (
     <nav>
-      <NavLink className='new-prompt-link' to='/'>New Prompt</NavLink>
-      <NavLink className='random-prompt-link' to='/Random-prompts-link'>Random prompts</NavLink>
-      <NavLink className='about-link' to='/about'>About</NavLink>
-    
+      <NavLink className="new-prompt-link" to="/">
+        New Prompt
+      </NavLink>
+      <NavLink className="random-prompt-link" to="/randomprompts">
+        Random prompts
+      </NavLink>
+      <NavLink className="about-link" to="/about">
+        About
+      </NavLink>
     </nav>
-  )
-
-
-}
+  );
+};
 
 export default Nav;
