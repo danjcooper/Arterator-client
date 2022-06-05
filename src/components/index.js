513e104b984e675b85547d7b4611a5571a807222
deb0890 <deborahf1@hotmail.com>
2022-06-05 14:58:31 +0100

    export randomimage component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
-export { default as Header } from './Header';
-export { default as PromptForm} from './PromptForm';
-export { default as CategorySelect } from './CategorySelect';
-export { default as PromptResult } from './PromptResult';
-export { default as Nav } from './Nav';
+export { default as Header } from "./Header";
+export { default as PromptForm } from "./PromptForm";
+export { default as CategorySelect } from "./CategorySelect";
+export { default as PromptResult } from "./PromptResult";
+export { default as Nav } from "./Nav";
+export { default as RandomImage } from "./RandomImage";
