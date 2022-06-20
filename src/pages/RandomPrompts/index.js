9dfee401560c26308bd3db7fab6d9c39adb25f57
deb0890 <deborahf1@hotmail.com>
2022-06-20 19:24:47 +0100

    add useState hook for randomword incoming

--- src/pages/RandomPrompts/index.js
+++ src/pages/RandomPrompts/index.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { RandomImage } from "../../components";
+import { RandomImage, RandomWord } from "../../components";
 import axios from "axios";
 import "./style.css";
 
 const imagesURL =
   "https://api.unsplash.com/photos/random?client_id=KlqKTIHfc9rS-ilCMxEZx_0-XF5g8PKXySEU5IiCVOM";
+const wordsURL = "https://random-word-api.herokuapp.com/word";
 
 const RandomPrompts = () => {
   const [randomImage, setRandomImage] = useState();
+  const [randomWord, setRandomWord] = useState();
   const [errorMessage, setErrorMessage] = useState();
 
   useEffect(() => {
